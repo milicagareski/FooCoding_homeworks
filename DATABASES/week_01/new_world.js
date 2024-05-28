6faa22f2fdee6505da44fb3f7928433f00deedc9
Milica Gareski <milicakiprovska77@gmail.com>
2024-05-28 20:36:38 +0200

    homework databases finished

--- DATABASES/week_01/new_world.js
+++ DATABASES/week_01/new_world.js
@@ -27,7 +27,7 @@ fs.readFile("new_world.sql", "utf8", (err, data) => {
     // QUESTION NUMBER 1
 
     connection.query(
-      "SELECT Name FROM country WHERE Population > 8000000",
+      "SELECT Name, Population FROM country WHERE Population > 8000000",
       (err, results) => {
         if (err) {
           console.error("error", err);
@@ -57,7 +57,7 @@ fs.readFile("new_world.sql", "utf8", (err, data) => {
             // QUESTION NUMBER 3
 
             connection.query(
-              "SELECT Name FROM city WHERE Population BETWEEN 500000 and 1000000",
+              "SELECT Name, Population FROM city WHERE Population BETWEEN 500000 and 1000000",
               (err, results) => {
                 if (err) {
                   console.error("error", err);
@@ -84,7 +84,75 @@ fs.readFile("new_world.sql", "utf8", (err, data) => {
                       results
                     );
 
-                    connection.end();
+                    // QUESTION NUMBER 5
+
+                    connection.query(
+                      "SELECT country.Name, country.Population FROM country JOIN city ON country.Code = city.CountryCode GROUP BY city.CountryCode HAVING COUNT(city.ID) > 10 AND SUM(city.Population) > 50000000;",
+                      (err, results) => {
+                        if (err) {
+                          console.error("error", err);
+                          return;
+                        }
+
+                        console.log(
+                          "Countries with city population more than 50000000:",
+                          results
+                        );
+
+                        // QUESTION NUMBER 6
+
+                        connection.query(
+                          "SELECT city.Name, city.Population FROM city WHERE city.Population > 5000000 AND city.CountryCode IN (SELECT country.Code FROM country  JOIN city ON country.Code = city.CountryCode GROUP BY country.Code HAVING COUNT(city.ID) > 10 AND SUM(city.Population) > 50000000);",
+                          (err, results) => {
+                            if (err) {
+                              console.error("error", err);
+                              return;
+                            }
+
+                            console.log(
+                              "Countries with city population more than 5000000:",
+                              results
+                            );
+
+                            // QUESTION NUMBER 7
+
+                            connection.query(
+                              "SELECT country.Name AS CountryName, country.Continent, city.Name AS CapitalName FROM country JOIN city ON country.Capital = city.ID WHERE  country.Population / country.SurfaceArea > 1000;",
+                              (err, results) => {
+                                if (err) {
+                                  console.error("error", err);
+                                  return;
+                                }
+
+                                console.log(
+                                  "Countries with a population density > 1 000 people / km2:",
+                                  results
+                                );
+
+                                // QUESTION NUMBER 8
+
+                                connection.query(
+                                  "SELECT Continent, Name AS Country, SurfaceArea FROM country WHERE Continent != 'Antarctica' AND SurfaceArea = (SELECT MAX(SurfaceArea) FROM country AS c WHERE c.Continent = country.Continent);",
+                                  (err, results) => {
+                                    if (err) {
+                                      console.error("error", err);
+                                      return;
+                                    }
+
+                                    console.log(
+                                      "Largest country (by size) on each continent, except Antarctica",
+                                      results
+                                    );
+
+                                    connection.end();
+                                  }
+                                );
+                              }
+                            );
+                          }
+                        );
+                      }
+                    );
                   }
                 );
               }
